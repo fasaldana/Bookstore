e98eafa8608dcd9ccce52e599fbcd167c9b72c81
fasaldana <fernando_s_98@hotmail.com>
2022-04-30 16:21:40 -0500

    Changed repeated code with mapping function

--- src/Components/bookList.js
+++ src/Components/bookList.js
@@ -1,22 +1,23 @@
 import Book from './Book';
 
-const BookList = () => (
-  <>
-    <div className="book-list">
-      <p>Category</p>
-      <div className="book-desc">
-        <Book title="The Hunger Games" author="Suzanne Collins" />
-      </div>
-      <button type="button">Remove</button>
-    </div>
-    <div className="book-list">
-      <p>Category</p>
-      <div className="book-desc">
-        <Book title="Dune" author="Frank Herbert" />
-      </div>
-      <button type="button">Remove</button>
-    </div>
-  </>
-);
+const BookList = () => {
+  const booksArr = [
+    { title: 'The Hunger Games', author: 'Suzanne Collins' },
+    { title: 'The Hunger Games2', author: 'Suzanne Collins2' },
+  ];
+  return (
+    <>
+      {booksArr.map((book) => (
+        <div key={book.name} className="book-list">
+          <p>Category</p>
+          <div className="book-desc">
+            <Book title={book.title} author={book.author} />
+          </div>
+          <button type="button">Remove</button>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default BookList;
