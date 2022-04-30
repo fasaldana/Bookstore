d6ad109b68e1501b4bc532a0b3e7efd3e04bf9e1
fasaldana <fernando_s_98@hotmail.com>
2022-04-30 16:25:20 -0500

    Move addbook inside booklist component

--- src/Components/bookList.js
+++ src/Components/bookList.js
@@ -1,3 +1,4 @@
+import AddBook from './AddBook';
 import Book from './Book';
 
 const BookList = () => {
@@ -15,6 +16,7 @@ const BookList = () => {
           </div>
         </div>
       ))}
+      <AddBook />
     </>
   );
 };
--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -1,11 +1,9 @@
-import AddBook from '../Components/AddBook';
 import BookList from '../Components/bookList';
 
 export default function Books() {
   return (
     <div>
       <BookList />
-      <AddBook />
     </div>
   );
 }
