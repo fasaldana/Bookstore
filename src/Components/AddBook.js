3c8c109bb64fba1c26c9f103aef04a2c28b87684
fasaldana <fernando_s_98@hotmail.com>
2022-05-05 15:44:13 -0500

    basic redux setup

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,12 +1,29 @@
-const AddBook = () => (
-  <div className="add-form">
-    <h2>ADD NEW BOOK</h2>
-    <form className="form">
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Author" />
-      <button type="button">ADD BOOK</button>
-    </form>
-  </div>
-);
+import { useState } from 'react';
+
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  return (
+    <div className="add-form">
+      <h2>ADD NEW BOOK</h2>
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onInput={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onInput={(e) => setAuthor(e.target.value)}
+        />
+        <button type="button">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
