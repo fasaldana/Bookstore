13de83ee5d72e4ff3c734f7a57ec8e1dea2ad7be
fasaldana <fernando_s_98@hotmail.com>
2022-05-05 17:21:34 -0500

    Fix error for missing id

--- src/Components/bookList.js
+++ src/Components/bookList.js
@@ -3,13 +3,13 @@ import Book from './Book';
 
 const BookList = () => {
   const booksArr = [
-    { title: 'The Hunger Games', author: 'Suzanne Collins' },
-    { title: 'The Hunger Games2', author: 'Suzanne Collins2' },
+    { id: 1, title: 'The Hunger Games', author: 'Suzanne Collins' },
+    { id: 2, title: 'The Hunger Games2', author: 'Suzanne Collins2' },
   ];
   return (
     <>
       {booksArr.map((book) => (
-        <div key={book.name} className="book-list">
+        <div key={book.id} className="book-list">
           <p>Category</p>
           <div className="book-desc">
             <Book title={book.title} author={book.author} />
